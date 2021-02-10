8e8798de1db99cce9a7f222e296b0743352030a5
Nadejda <mezz@74.ru>
2021-02-10 15:54:31 +0500

    тест дизайн+рпостой калькулятор на 1 строку работает

--- app/src/main/java/com/example/resept/MainActivity.java
+++ app/src/main/java/com/example/resept/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.resept;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick (View v) {
+        EditText el1 = (EditText)findViewById(R.id.Porsiy);
+        EditText el2 = (EditText)findViewById(R.id.Resept1);
+        TextView resText1 = (TextView)findViewById(R.id.Nado1);
+
+        int num1 = Integer.parseInt((el1.getText().toString()));
+        int num2 = Integer.parseInt((el2.getText().toString()));
+        int resProizv1 = num1*num2;
+        resText1.setText(Integer.toString(resProizv1));
     }
 }
