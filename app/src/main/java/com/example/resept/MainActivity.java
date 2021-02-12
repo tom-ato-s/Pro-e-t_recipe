b311260a78cf9293f6b12d8f33b7e5245e3c6224
Nadejda <mezz@74.ru>
2021-02-12 10:07:45 +0500

    2-й обработчик кнопокб все строки, работает

--- app/src/main/java/com/example/resept/MainActivity.java
+++ app/src/main/java/com/example/resept/MainActivity.java
@@ -11,7 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText el;
-    private EditText el1, resText1;
+    private EditText el1, el2, el3, el4, el5, el6, resText1;
+    private TextView resText2, resText3, resText4, resText5, resText6;
     private Button button_;
 
     @Override
@@ -24,7 +25,17 @@ public class MainActivity extends AppCompatActivity {
     private void addListennerOnButton() {
         el = (EditText)findViewById(R.id.Porsiy);
         el1 = (EditText)findViewById(R.id.Resept1);
+        el2 = (EditText)findViewById(R.id.Resept2);
+        el3 = (EditText)findViewById(R.id.Resept3);
+        el4 = (EditText)findViewById(R.id.Resept4);
+        el5 = (EditText)findViewById(R.id.Resept5);
+        el6 = (EditText)findViewById(R.id.Resept6);
         resText1 = (EditText)findViewById(R.id.Nado1);
+        resText2 = (TextView)findViewById(R.id.Nado2);
+        resText3 = (TextView)findViewById(R.id.Nado3);
+        resText4 = (TextView)findViewById(R.id.Nado4);
+        resText5 = (TextView)findViewById(R.id.Nado5);
+        resText6 = (TextView)findViewById(R.id.Nado6);
         button_ = (Button)findViewById(R.id.button);
         button_.setOnClickListener(
                 new View.OnClickListener() {
@@ -32,8 +43,23 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         int num = Integer.parseInt((el.getText().toString()));
                         int num1 = Integer.parseInt((el1.getText().toString()));
+                        int num2 = Integer.parseInt((el2.getText().toString()));
+                        int num3 = Integer.parseInt((el3.getText().toString()));
+                        int num4 = Integer.parseInt((el4.getText().toString()));
+                        int num5 = Integer.parseInt((el5.getText().toString()));
+                        int num6 = Integer.parseInt((el6.getText().toString()));
                         int resProizv1 = num*num1;
+                        int resProizv2 = num*num2;
+                        int resProizv3 = num*num3;
+                        int resProizv4 = num*num4;
+                        int resProizv5 = num*num5;
+                        int resProizv6 = num*num6;
                         resText1.setText(Integer.toString(resProizv1));
+                        resText2.setText(Integer.toString(resProizv2));
+                        resText3.setText(Integer.toString(resProizv3));
+                        resText4.setText(Integer.toString(resProizv4));
+                        resText5.setText(Integer.toString(resProizv5));
+                        resText6.setText(Integer.toString(resProizv6));
                     }
                 }
         );
